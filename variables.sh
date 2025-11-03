2ee9348c6055fa314f82bab54a475ed7a67c3f25
Buela <chbuela22@gmail.com>
2025-11-03 17:50:56 +0530

    variables

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=$1
+person2=$2
+
+echo "$person1 :: welcome to india"
+echo "$person2 :: thank u nice to meet uh"
+echo "$person1 :: so can we have meeting?"
+echo "$person2 :: ofcourse"
